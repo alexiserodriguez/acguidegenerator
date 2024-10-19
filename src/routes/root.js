0db257b9233e1cadd685f81aba739b05e39827fa
Alexis <alexisrod1997@gmail.com>
2024-10-19 19:52:55 -0400

    customization

--- src/routes/root.js
+++ src/routes/root.js
@@ -3,15 +3,7 @@ export default function Root() {
     <>
       <div id="sidebar">
         <h1>Please use a correct URI</h1>
-        <h2>Proper Endpoints</h2>
-        <p>
-          <b>bullmetal.app/lotLabel</b>
-          <br></br>{" "}
-          Example:/lotLabel?lotId=dHVhjE7f28cJ3NNNB27Dsu&sku=PAINT0016&jobName=GARD%2010574&jobId=23717&release=1of1&kit=23717-R1-K2
-          <br></br>
-          <b>bullmetal.app/kitLabel</b> - under development
-          <b>bullmetal.app/deviceLabel</b> - under development
-        </p>
+       
       </div>
     </>
   );
